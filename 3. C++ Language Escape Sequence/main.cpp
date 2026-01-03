5c821e167f67fd8a9f2b50e2e6ec6547022e50e7
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-03 20:48:05 +0600

    Add files via upload

---
+++ 3. C++ Language Escape Sequence/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Escape Sequence";
+  return 0;
+}
+
+//use of \n
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello User!\n";
+    cout << "Welcome to the world of C++.\n";
+    cout << "Line 1\nLine 2\nLine 3";
+
+    return 0;
+}
+
+//use of \t
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "ID\tName\tSubject" << endl;
+    cout << "--------------------------" << endl;
+
+
+    cout << "101\tMohammad\tMath" << endl;
+    cout << "102\tAbdullah\tPhysics" << endl;
+    cout << "103\tAziz\tEnglish" << endl;
+
+    return 0;
+}
+
+//use of \\ backslash
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "This is a backslash: \\" << endl;
+    cout << "File Location: C:\\Users\\Desktop\\MyCode.cmm" << endl;
+
+    return 0;
+}
+
+//use of \"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "She said, \"I love programming!\"" << endl;
+    cout << "The book name is \"C++ Guide\"." << endl;
+
+    return 0;
+}
+
+
