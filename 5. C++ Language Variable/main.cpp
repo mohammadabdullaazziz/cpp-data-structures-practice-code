30839cd0988e42c815c1576d82a7426023f2248d
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-08 20:51:36 +0600

    C++ Language Variable

---
+++ 5. C++ Language Variable/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int age = 29;          // integer
+    float price = 10.50;   // float number 
+    char grade = 'A';      // character
+    string name = "Abdullah"; // text
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    return 0;
+}
