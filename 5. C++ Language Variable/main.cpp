d70b373e70a792401f502d2e4f99d25dea866469
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-11 19:49:56 +0600

    Modify variable
    Changed 'float' to 'double' for price variable and added a boolean variable.

--- 5. C++ Language Variable/main.cpp
+++ 5. C++ Language Variable/main.cpp
@@ -5,11 +5,12 @@ int main()
 
 {
     int age = 29;          // integer
-    float price = 10.50;   // float number 
+    double price = 10.50;   // float number 
     char grade = 'A';      // character
     string name = "Abdullah"; // text
-
+    bool myBoolean = true;  //bool
     cout << "Name: " << name << endl;
     cout << "Age: " << age << endl;
     return 0;
+
 }
