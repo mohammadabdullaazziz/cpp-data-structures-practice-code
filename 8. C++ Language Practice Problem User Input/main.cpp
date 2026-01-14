efae37ca071e1f0fcdce69763ce14b751bc7cb0b
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-14 23:55:57 +0600

    C++ Language Practice Problem User Input

---
+++ 8. C++ Language Practice Problem User Input/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   int num;
+   cout << "Enter Your Number: ";
+   cin >> num;
+   cout << "You entered: " << num <<endl;
+
+   int a, b, sum;
+   cout << "Type a Number: ";
+   cin >> a ;
+   cout << "Type another Number: ";
+   cin >> b;
+   sum = a + b;
+   cout << "Sum is:" <<sum <<endl;
+
+   string name;
+   int age;
+   cout << "Enter Your Name: ";
+   getline(cin, name);
+   cout << "Enter Your Age: ";
+   cin >> age;
+   cout << "Hello " << name << ", your age is " << age <<endl;
+
+   int length, width;
+   cout << "Enter your length: ";
+   cin >> length;
+   cout << "Enter your width: ";
+   cin >> width;
+   cout << "Area = " << length * width <<endl;
+
+   int a, b, c;
+   float average;
+   cout << "1st Person: ";
+   cin >> a;
+   cout << "2nd Person: ";
+   cin >> b;
+   cout << "3rd Person: ";
+   cin >> c;
+   average = (a + b + c) / 3.0;
+   cout << "Average = " << average;
+   return 0;
+
+}
