2cd8d5d74ba8e7cadd87e780ac178aa1a252744f
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-19 21:09:59 +0600

    C++ Language User Input

---
+++ 7. C++ Language User Input/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   string name;
+   cout << "Name: ";
+   cin >> name;
+   cout << "Your name is: " << name << endl;
+
+   int age;
+   cout << "Age: ";
+   cin >> age;
+   cout << "Your age is: " << age <<endl;
+
+   char ch;
+   cout << "Enter Character: ";
+   cin >> ch;
+   cout << "Your Character is: " << ch;
+   return 0;
+
+}
