5d1fe760c3663b83bb0e4b298057e2dc3eb31396
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-24 22:01:36 +0600

    variable examples
    Added a new main function with additional variable declarations and outputs.

--- 5. C++ Language Variable/main.cpp
+++ 5. C++ Language Variable/main.cpp
@@ -14,3 +14,21 @@ int main()
     return 0;
 
 }
+
+#include <iostream>
+using namespace std;
+int main(){
+   string name = "Mohammad";
+   int age = 30;
+   float height = 5.2;
+   string village = "Changmary";
+   name = "Abdullah";
+   string subject = "programming";
+   bool isPass = true;
+   char grade = 'A';
+   cout << "My name is: " <<name << " My age is: " <<age << " My height is: " <<height << " My village is: " <<village <<endl;
+   cout << "My favourite subject is: " <<subject <<endl;
+   cout << "My grade is: " <<grade <<endl;
+   cout << "i am " <<isPass <<endl;
+   return 0;
+}
