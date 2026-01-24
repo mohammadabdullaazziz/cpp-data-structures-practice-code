d7ac6491ce6a30d6d9500e977a7793ee20290f52
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-24 22:37:24 +0600

    user input collection added.
    Updated user input program to collect additional details such as full name, height, village, subject, CGPA, and grade.

--- 7. C++ Language User Input/main.cpp
+++ 7. C++ Language User Input/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 int main(){
@@ -17,5 +18,54 @@ int main(){
    cin >> ch;
    cout << "Your Character is: " << ch;
    return 0;
+}
+*/
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fullName;
+    cout << "Enter your full-name: ";
+    getline(cin, fullName);
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    float height;
+    cout << "Enter your height: ";
+    cin >> height;
+
+    cin.ignore(); 
+
+    string village;
+    cout << "Enter your village: ";
+    getline(cin, village);
+
+    string subject;
+    cout << "Enter your subject: ";
+    getline(cin, subject);
+
+    float cgpa;
+    cout << "Enter your cgpa: ";
+    cin >> cgpa;
+
+    char grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+
+    cout << "\n--- Output ---\n";
+
+    cout << "Name: " << fullName << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Village: " << village << endl;
+    cout << "Subject: " << subject << endl;
+    cout << "CGPA: " << cgpa << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
 }
